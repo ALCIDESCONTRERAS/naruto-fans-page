373128c8381278b11eb8e0ec9a6d80da22e70ef4
Alcides <alcidescon@gmail.com>
2025-07-25 17:07:32 +0200

    guardado para las rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+import { getCharacters } from "./services/narutoApi";
+import type { Characters } from "./interfaces/characters.interface";
+
 function App() {
-  return (
-    <>
-      <h1>App</h1>
-    </>
-  )
+  const [charactersApi, setCharactersApi] = useState<Characters | null>(null);
+
+  const getCharactersApi = async () => {
+    const data = await getCharacters();
+    if (data) {
+      setCharactersApi(data!);
+    }
+  };
+
+  console.log(charactersApi);
+
+  useEffect(() => {
+    getCharactersApi();
+  }, []);
+
+  return <>
+  {
+    charactersApi?.characters.map(character => (
+      <article key={character.id} className="flex flex-col">
+        <img className="w-46" src={character.images[0]} alt="" />
+        <span>{character.name}</span>
+        <span>{character.personal.sex}</span>
+        <span>{character.personal.birthdate}</span>
+        <span>{character.personal.affiliation}</span>
+        <span>{character.personal.clan}</span>
+      </article>
+    ))
+    }
+  </>;
 }
 
-export default App
+export default App;
